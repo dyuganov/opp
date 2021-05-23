0ad243851e750c57f75c3136ca265db7dadcc586
dyuganov-n <dyuganov.n@gmail.com>
2021-05-23 19:12:00 +0700

    getting task changes

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -23,6 +23,9 @@
 #define TASK_RECEIVED (44)
 #define WORK_DONE (55)
 
+#define REQUEST_TAG (101)
+#define ANSWER_TAG (102)
+
 using namespace std;
 
 class Task{
@@ -47,6 +50,11 @@ public:
     size_t getTasksNum(){ return tasks.size(); }
 };
 
+TaskList taskList;
+pthread_mutex_t taskListMutex;
+long double calculations = 0;
+size_t iterationCnt = 0;
+
 void generateTasks(TaskList& taskList, size_t& iterationCnt, pthread_mutex_t& taskListMutex, const int& rank, const int& size){
     pthread_mutex_lock(&taskListMutex);
     for (int i = rank * TASK_LIST_SIZE; i < (rank + 1) * TASK_LIST_SIZE; i++){
@@ -69,12 +77,57 @@ void initMPI(int* rank, int* size){
     MPI_Comm_rank(MPI_COMM_WORLD, rank);
 }
 
+bool isAttrError(const int& attrResult){
+    if(attrResult != 0){
+        fprintf(stderr, "Can't init attrs\n");
+        MPI_Finalize();
+        return true;
+    }
+    return false;
+}
+
+bool isMutexError(const int& mutexResult){
+    if(mutexResult != 0){
+        fprintf(stderr, "Can't init mutex\n");
+        MPI_Finalize();
+        return true;
+    }
+    return false;
+}
+
+int getNewTask(int supplier){
+    int msgToSend = TASK_NEED;
+    int msgToReceive = 0;
+
+    MPI_Send(&msgToSend, 1, MPI_INT, supplier, REQUEST_TAG, MPI_COMM_WORLD);
+    MPI_Recv(&msgToReceive, 1, MPI_INT, supplier, ANSWER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    if(msgToReceive == TASK_END) return 0;
+
+    if(msgToReceive == TASK_SEND){
+        int receiveTasksCnt = 0;
+        pthread_mutex_lock(&taskListMutex);
+        MPI_Recv(&receiveTasksCnt, 1, MPI_INT, supplier, ANSWER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&tasklist[sponsor * TASKS_IN_LIST], receiveTasksCnt, MPI_INT, supplier, ANSWER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        given_task = supplier * TASKS_IN_LIST; // разобраться в логике передачи, исправить
+        pthread_mutex_unlock(&taskListMutex);
+        return 1;
+    }
+    msgToSend = TASK_RECEIVED;
+    MPI_Send(&msgToSend, 1, MPI_INT, supplier, REQUEST_TAG, MPI_COMM_WORLD);
+}
+
+void* resolveTask(void* args){
+
+}
+
+void* sendTask(void* args){
+
+}
 
 int main(int argc, char* argv[]) {
     int provided;
-    pthread_attr_t attrs;
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-
     if (provided != MPI_THREAD_MULTIPLE){
         std::cout << "Can't init thread" << std::endl;
         MPI_Finalize();
@@ -83,21 +136,39 @@ int main(int argc, char* argv[]) {
     int size = 0, rank = 0;
     initMPI(&rank, &size);
 
+    pthread_attr_t attrs;
+    if(isMutexError(pthread_mutex_init(&taskListMutex, nullptr))) {
+        pthread_attr_destroy(&attrs);
+        pthread_mutex_destroy(&taskListMutex);
+        return 0;
+    }
+    if(isAttrError(pthread_attr_init(&attrs))) {
+        pthread_attr_destroy(&attrs);
+        pthread_mutex_destroy(&taskListMutex);
+        return 0;
+    }
+    if(isAttrError(pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_JOINABLE))) {
+        pthread_attr_destroy(&attrs);
+        pthread_mutex_destroy(&taskListMutex);
+        return 0;
+    }
+
     double timeStart = 0, timeFinish = 0;
     if(rank == 0) timeStart = MPI_Wtime();
 
-    TaskList taskList;
-    pthread_mutex_t taskListMutex;
-    long double calculations = 0;
-    size_t iterationCnt = 0;
-
-
-
+    pthread_t sendingThread;
+    pthread_t resolvingTasksThread;
+    pthread_create(&sendingThread, &attrs, sendTask, nullptr);
+    pthread_create(&resolvingTasksThread, &attrs, resolveTask, nullptr);
+    pthread_join(sendingThread, nullptr);
+    pthread_join(resolvingTasksThread, nullptr);
 
     if(rank == 0) {
         timeFinish = MPI_Wtime();
         cout << "Time: " << timeFinish - timeStart << endl;
     }
+    pthread_attr_destroy(&attrs);
+    pthread_mutex_destroy(&taskListMutex);
     MPI_Finalize();
     return 0;
 }
